8a2db3ff9132694d9064ac8e78a6518db6bbc1fd
Lawkh <marcelomur92@gmail.com>
2025-05-08 16:30:48 +0200

    Update MyRule.kt
    Changed base rule to make 1 function interfaces to be noted as fun interfaces.

--- src/main/kotlin/org/example/detekt/MyRule.kt
+++ src/main/kotlin/org/example/detekt/MyRule.kt
@@ -9,19 +9,44 @@ import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import org.jetbrains.kotlin.psi.KtClass
 
-class MyRule(config: Config) : Rule(config) {
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
+
+class FunctionOnlyInterfaceRule(config: Config) : Rule(config) {
+
     override val issue = Issue(
-        javaClass.simpleName,
-        Severity.CodeSmell,
-        "Custom Rule",
-        Debt.FIVE_MINS,
+        id = "FunctionOnlyInterface",
+        severity = Severity.Style,
+        description = "Interface with only 1 function should be declared as 'fun interface'",
+        debt = Debt.FIVE_MINS
     )
 
     override fun visitClass(klass: KtClass) {
         super.visitClass(klass)
 
-        if (klass.isInner()) {
-            report(CodeSmell(issue, Entity.atName(klass), "Custom message"))
+        if (!klass.isInterface() || klass.isFun) return
+
+        val functions = klass.getBody()
+            ?.declarations
+            ?.filterIsInstance<KtNamedFunction>()
+            ?.filter { it.hasModifier(KtTokens.ABSTRACT_KEYWORD) || it.bodyExpression == null }
+            ?: return
+
+        val properties = klass.getBody()
+            ?.declarations
+            ?.filterIsInstance<KtProperty>()
+            ?.filter { it.hasModifier(KtTokens.ABSTRACT_KEYWORD) }
+            ?: return
+
+        if (functions.size == 1 && properties.isEmpty()) {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.atName(klass),
+                    message = "This interface must be 'fun interface'."
+                )
+            )
         }
     }
 }
